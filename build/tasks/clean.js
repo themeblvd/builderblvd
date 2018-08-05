6d5656f1285a6c60589fb0b35a26ef0d62de2ff5
Jason Bobich <jasonbobich@gmail.com>
2018-08-05 07:36:43 -0600

    Fix zip not getting deleted in clean Gulp task.

--- build/tasks/clean.js
+++ build/tasks/clean.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const del = require('del');
 
 function clean() {
-  return del(['../assets/css', '../assets/js', '*.zip'], { force: true });
+  return del(['../assets/css', '../assets/js', '../*.zip'], { force: true });
 }
 
 module.exports = clean;
